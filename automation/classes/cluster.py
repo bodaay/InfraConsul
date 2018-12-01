e48da4897fb93f4ec8c3437de5f87e5d034afcdf
bodaay <khalefa@addrek.com>
2018-12-01 22:08:40 +0300

    ok

--- automation/classes/cluster.py
+++ automation/classes/cluster.py
@@ -1,3 +1,19 @@
+import os
+
+
 class cluster:
     def __init__(self):
         self.ClusterName = None
+        self.OrganizationDomainName = None
+
+    def SelfCheck(self):
+        if self.ClusterName is None or self.ClusterName == "":
+            return False, "Cluster Name is Empty or not set"
+        if self.OrganizationDomainName is None:
+            return False, "Organization Domain Name is Empty or not set"
+        return True
+
+    def InitNewCluster(self):
+        if os.path.exists(self.OrganizationDomainName):
+            return False, "Cluster Working Folder \"%s\" Already Exists" % self.OrganizationDomainName
+        return True
